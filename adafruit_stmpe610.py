bb2f73058d328879d17aeb494c26fb2c18bd8437
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:14 -0400

    Update version string

--- adafruit_stmpe610.py
+++ adafruit_stmpe610.py
@@ -27,7 +27,7 @@ import time
 from micropython import const
 
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_STMPE610.git"
 
 
